3e129aa48db6da26cb4ae0849f497c37aefcc4bf
ratigumashvili <ratigumashvili@gmail.com>
2025-04-10 10:56:17 +0400

    small fix

--- src/app/[locale]/_components/markdown-content.jsx
+++ src/app/[locale]/_components/markdown-content.jsx
@@ -6,10 +6,11 @@ export default function MarkDownContent({ markdown }) {
   return (
     <div className='[&_h3]:mb-4 [&_h3]:text-xl [&_h4]:my-6 [&_p]:mb-4 [&_ul]:mb-3 [&_li]:list [&_table]:table [&_tr]:border-b [&_td]:py-2 [&_td]:min-w-40 [&_td]:text-left'>
       <ReactMarkdown
-        children={markdown}
         remarkPlugins={[remarkGfm]}
         rehypePlugins={[rehypeRaw]}
-      />
+      >
+        {markdown}
+      </ReactMarkdown>
     </div>
   )
 }
